e7f6f5660779d323255ae2c5ca12d473c89f01b0
chinguyen98 <dacchi6761@gmail.com>
2020-11-13 15:29:47 +0700

    react responsive

---
+++ src/helpers/screen.helper.js
@@ -0,0 +1,8 @@
+export const screenMedia = {
+  desktopOrLaptop: { minDeviceWidth: 1224 },
+  bigScreen: { minDeviceWidth: 1824 },
+  tabletOrMobile: { maxWidth: 1224 },
+  tabletOrMobileDevice: { maxDeviceWidth: 1224 },
+  portrait: { orientation: 'portrait' },
+  retina: { minResolution: '2dppx' },
+}
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
+import { screenMedia } from '../../helpers/screen.helper';
+
+import res_imgone from '../../assets/images/res_img1.png';
+import res_imgtwo from '../../assets/images/res_img2.jpg';
 
 function Dashboard() {
+  const isDesktopOrLaptop = screenMedia.desktopOrLaptop;
+  const isMobileOrTablet = screenMedia.tabletOrMobile;
+
   return (
     <div className="dashboard pt-3">
       <h1>Dashboard!!!</h1>
+      <div>
+        <MediaQuery {...isDesktopOrLaptop}>
+          <img style={{ width: '50%', height: 'auto' }} src={res_imgone} alt="" />
+        </MediaQuery>
+        <MediaQuery {...isMobileOrTablet}>
+          <img style={{ width: '80%', height: 'auto' }} src={res_imgtwo} alt="" />
+        </MediaQuery>
+      </div>
     </div>
   )
 }
